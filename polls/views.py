6e6d2aba699488bb7eac60ae1cd9630b037c9a45
Jam <jdmaligad@gmail.com>
2019-01-21 20:02:11 +0800

    updated

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse # import HttpResponse
+from django.http import HttpResponse
 from .models import Question
+from .forms import QuestionModelForm
 
-# Create your views here.
-
-def index(request):
+def index(request): 
     context = {}
     questions = Question.objects.all()
     context['questions'] = questions
     return render(request, 'index.html', context)
 
 def help(request):
-    return HttpResponse('Test')
+    return render(request, 'This is the help page.')
+
+def detail(request, question_id):
+    context = {}
+    context['question'] = Question.objects.get(id=question_id)
+    return render(request, 'detail.html', context)
+
+def update(request, question_id):
+    if request.method == 'POST':
+        form = request.POST['form']
+    else:
+        question = Question.objects.get(id=question_id)
+        context = {}
+        context['form'] = QuestionModelForm(instance=question)
+        context['question'] = question
+        return render(request, 'update.html', context)
